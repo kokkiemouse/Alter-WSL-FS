b6221a06d1144ab012450b2817133e8738b40b96
kokkiemouse <39451248+kokkiemouse@users.noreply.github.com>
2020-05-17 20:16:39 +0900

    Update env.sh

--- env.sh
+++ env.sh
@@ -5,12 +5,8 @@ ROOTFS_VER="2020.04.01"
 ROOTFS_FN="archlinux-bootstrap-${ROOTFS_VER}-${ARCH}.tar.gz"
 ROOTFS_URL="http://mirrors.kernel.org/archlinux/iso/${ROOTFS_VER}/${ROOTFS_FN}"
 
-FRTCP_BLD="18082100"
-FRTCP_VER="1.23-1"
-FRTCP_FN="fakeroot-tcp-${FRTCP_VER}-${ARCH}.pkg.tar.xz"
-FRTCP_URL="https://github.com/yuk7/arch-prebuilt/releases/download/${FRTCP_BLD}/${FRTCP_FN}"
 
-PAC_PKGS="base less nano sudo vim curl iputils alterlinux-keyring alterlinux-calamares aptpac libpwquality yay-bin"
+PAC_PKGS="base less nano sudo vim curl iputils alterlinux-keyring alterlinux-calamares aptpac libpwquality yay-bin fakeroot"
 
 if type curl >/dev/null 2>&1 ;then
     DLR="curl"
