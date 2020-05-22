363631fd6f881c493548240998501a53a0fbd0c9
kokkiemouse <kokkiemouse@gmail.com>
2020-05-23 08:26:02 +0900

    user_setup script added

---
+++ user_setup.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+if [ $# -ne 1 ]; then
+    echo "Alter.exeのパスを入力してください。"
+    exit 1
+fi
+function ask_yes_no {
+  while true; do
+    echo -n "$* [y/n]: "
+    read ANS
+    case $ANS in
+      [Yy]*)
+        return 0
+        ;;  
+      [Nn]*)
+        return 1
+        ;;
+      *)
+        echo "yまたはnを入力してください"
+        ;;
+    esac
+  done
+}
+EXE_PATH=`wslpath -u $1`
+
+if ask_yes_no "ユーザーを作成します。よろしいですか?"; then
+  # ここに「Yes」の時の処理を書く
+  echo "Yes"
+  echo -n "ユーザー名: "
+  read Username
+  if ask_yes_no "$Username このユーザー名でよろしいですか?"; then
+    echo useradd -m -g wheel -s /bin/bash ${Username}
+    echo "パスワードを設定します。"
+    echo passwd ${Username}
+    echo "デフォルトユーザーを設定しています"
+    echo ${EXE_PATH} config --default-user ${Username}
+  fi
+else
+  # ここに「No」の時の処理を書く
+  echo "No"
+fi
