84c9f9f832b08929fb61a8bea575453036bba195
kokkiemouse <kokkiemouse@gmail.com>
2020-05-23 09:32:23 +0900

    os-release add

--- env.sh
+++ env.sh
@@ -4,8 +4,7 @@ ARCH="x86_64"
 ROOTFS_VER="2020.05.01"
 ROOTFS_FN="archlinux-bootstrap-${ROOTFS_VER}-${ARCH}.tar.gz"
 ROOTFS_URL="http://mirrors.kernel.org/archlinux/iso/${ROOTFS_VER}/${ROOTFS_FN}"
-
-
+OSRELEASE_URL="https://raw.githubusercontent.com/FascodeNet/alterlinux/dev-stable/channels/share/airootfs.any/usr/lib/os-release"
 PAC_PKGS="base automake	autoconf gcc make pkgconf which m4 groff grep bison findutils less nano sudo vim curl iputils alterlinux-keyring alterlinux-calamares aptpac libpwquality yay-bin fakeroot"
 
 if type curl >/dev/null 2>&1 ;then
